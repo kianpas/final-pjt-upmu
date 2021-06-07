cacb0abc9711e810b9029adbb85534a292b58fbb
AlexanderFishcake <blucency@gmail.com>
2021-06-07 21:30:29 +0900

    2021-06-07

---
+++ src/main/java/com/fpjt/upmu/document/model/dao/DocDao.java
@@ -0,0 +1,5 @@
+package com.fpjt.upmu.document.model.dao;
+
+public interface DocDao {
+
+}
---
+++ src/main/java/com/fpjt/upmu/document/model/service/DocServiceImpl.java
@@ -0,0 +1,8 @@
+package com.fpjt.upmu.document.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocServiceImpl implements DocService {
+
+}
