aa410825bf8fa1f62e88165de58f89d36b4fde68
AlexanderFishcake <blucency@gmail.com>
2021-06-07 23:44:34 +0900

    2021-06-07
    db접속

--- src/main/java/com/fpjt/upmu/document/controller/DocController.java
+++ src/main/java/com/fpjt/upmu/document/controller/DocController.java
@@ -1,11 +1,20 @@
 package com.fpjt.upmu.document.controller;
 
+import java.beans.PropertyEditor;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fpjt.upmu.document.model.service.DocService;
+import com.fpjt.upmu.document.model.vo.Document;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,8 +28,24 @@ public class DocController {
 	
 	@GetMapping("/docList.do")
 	public String docList() {
-		log.debug("테스트");
+		log.info("test");
+		List<Document> docList = docService.selectDocList();
+		log.info("docList = {}", docList);
+		
 		return "document/docList";
 	}
 
+	
+	/**
+	 * java.sql.Date, java.util.Date 필드에 값 대입시
+	 * 사용자 입력값이 ""인 경우, null로 처리될 수 있도록 설정.
+	 * @param binder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		//org.springframework.beans.propertyeditors.CustomDateEditor.CustomDateEditor(DateFormat dateFormat, boolean allowEmpty)
+		PropertyEditor editor = new CustomDateEditor(format,true); //빈 문자열을 허용하겠다는 뜻.
+		binder.registerCustomEditor(Date.class, editor);
+	}
 }
