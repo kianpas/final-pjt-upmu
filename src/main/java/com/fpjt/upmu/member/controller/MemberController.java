d6fb19db6b129556b32a12d9832bd13b95ada940
miiin_sseong <70365399+alstjd0051@users.noreply.github.com>
2021-06-21 19:43:50 +0900

    Revert "no commit message"
    This reverts commit 8f6f000d6b364aef37c63a1449c6c498aaafb157.

--- src/main/java/com/fpjt/upmu/member/controller/MemberController.java
+++ src/main/java/com/fpjt/upmu/member/controller/MemberController.java
@@ -66,7 +66,6 @@ public class MemberController {
 	
 	@GetMapping("/memberEnroll.do")
 	public void memberEnroll() {
-		
 	}
 	
 	@PostMapping("/memberEnroll.do")
@@ -80,8 +79,9 @@ public class MemberController {
 			member.setEmp_pw(encodedPassword);
 			log.info("member(암호화처리이후) = {}", member);
 			
+			//1. 업무로직
 			int result = memberService.insertMember(member);
-			
+			//2. 사용자피드백
 			redirectAttr.addFlashAttribute("msg", "회원가입성공");
 		} catch (Exception e) {
 			log.error("회원가입 오류!", e);
